8418d9b6a30ecad6b20eba4f5d908da722e9e876
BARRY27 <barry.on@hotmail.com>
2020-08-21 10:00:23 +0200

    first commit

---
+++ api.js
@@ -0,0 +1,11 @@
+const MongoCLient = require("mongodb").MongoClient;
+const express = require("express");
+const bodyParser = require("body-parser"); // use to parse the body in Json Format
+
+const url = "mongodb://localhost:27017";
+const app = express();
+
+const main = async () => {
+    /* MONGO conection and DataBase création*/
+    const client = await MongoCLient.connect(url, { useUnifiedTopology: true });
+    const dataBase = client.db("Assign");
